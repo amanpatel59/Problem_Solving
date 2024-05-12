3ff2aa5bce82cf7002fa4f0ccdf4de88d25f3d89
amanpatel59 <ap707359@gmail.com>
2024-05-12 16:58:40 +0530

    added new approaches

--- DP_1/house_robber.cpp
+++ DP_1/house_robber.cpp
@@ -1,88 +1,64 @@
+class Solution {
+public:
+    // PURE RECURSION
+    // WILL RESULT IN TLE
 
-// APPROACH 1
+    // int bestPoss(vector<int>&nums , int indx){
+    //     if(indx == 0)return nums[0];
+    //     if(indx < 0)return 0;
+    //     int pick = nums[indx]+bestPoss(nums,indx-2);
+    //     int notPick = bestPoss(nums,indx-1);
+    //     return max(pick , notPick);
+    // }
+    // int rob(vector<int>& nums) {
+    //     return bestPoss(nums,nums.size()-1);
+    // }
 
-// void robbing(int& ans , int sum , vector<int>&nums , int index)
-// {
-//     if(index+2 >= nums.size())
-//     {
-//         ans = max(ans,sum);
-//         return;
-//     }
-//     for(int i = index+2 ; i < nums.size() ; i++)
-//     {
-//         robbing(ans,sum+nums[i],nums,i);
-//     }
-//     return;
-// }
 
-// int rob(vector<int>& nums) {
-//     if(nums.size()==1)return nums[0];
-//     if(nums.size()==2)return max(nums[0],nums[1]);
+    // MEMOIZATION
 
-//     int ans = 0 , sum = 0 , index = 0;
-//     robbing(ans,nums[0],nums,0);
-//     // robbing(ans,nums[1],nums,1);
-//     return ans;
-// }
+    // int bestPoss(vector<int>&nums , int indx , vector<int>&dp){
+    //     if(indx == 0)return dp[0] = nums[0];
+    //     if(indx < 0)return 0;
+    //     if(dp[indx] != -1)return dp[indx];
+    //     int pick = nums[indx]+bestPoss(nums,indx-2,dp);
+    //     int notPick = bestPoss(nums,indx-1,dp);
+    //     return dp[indx] = max(pick , notPick);
+    // }
+    // int rob(vector<int>& nums) {
+    //     vector<int>dp(nums.size(),-1);
+    //     return bestPoss(nums,nums.size()-1,dp);
+    // }
 
-// APPROACH 2
 
-// int robbing(int index , vector<int>&nums)
-// {
-//     if (index >= nums.size()) {
-//         return 0;
-//     }
+    // TABULATION
+    // int rob(vector<int>& nums) {
+    //     vector<long long>dp(nums.size(),0);
+    //     dp[0] = nums[0];
+    //     for(int i = 1 ; i < nums.size() ; i++){
+    //         int pick = nums[i];
+    //         if(i-2 >= 0)pick = nums[i] + dp[i-2];
+    //         int notPick = dp[i-1];
+    //         dp[i] = max(pick,notPick);
+    //     }
+    //     return dp[nums.size()-1];
+    // }
 
-//     int robCurrent = nums[index] + robbing(index + 2, nums);
-//     int skipCurrent = robbing(index + 1, nums);
 
-//     return max(robCurrent,skipCurrent);
-// }
-// int rob(vector<int>& nums)
-// {
-//     if(nums.size() == 1)return nums[0];
-//     if(nums.size() == 2)return max(nums[0],nums[1]);
-
-//     return robbing(0,nums);
-// }
-
-// APPROACH 3 , ADDING MEMO IN APPROACH 2
-
-int robbingMemo(int index, vector<int> &nums, unordered_map<int, int> &memo)
-{
-    if (index >= nums.size())
-    {
-        return 0;
+    // SPACE OPTIMIZATION
+    // SINCE WE ONLY REQUIRE INFO ABOUT PAST 2 WE CAN HAVE VARS INSTEAD OF A FULL VECTOR
+    int rob(vector<int>& nums) {
+        int adjacent = nums[0];
+        int previousToAdjacent = nums[0];
+        int curr = nums[0];
+        for(int i = 1 ; i < nums.size() ; i++){
+            int pick = nums[i];
+            if(i-2 >= 0)pick = nums[i] + previousToAdjacent;
+            int notPick = adjacent;
+            curr = max(pick,notPick);
+            previousToAdjacent = adjacent;
+            adjacent = curr;
+        }
+        return curr;
     }
-
-    // Check if the result for the current index is already memoized
-    if (memo.find(index) != memo.end())
-    {
-        return memo[index];
-    }
-
-    // Rob the current house, skip the next, and move to the house after that
-    int robCurrent = nums[index] + robbingMemo(index + 2, nums, memo);
-
-    // Skip the current house and move to the next
-    int skipCurrent = robbingMemo(index + 1, nums, memo);
-
-    // Update memoization table with the maximum of robbing or skipping the current house
-    memo[index] = max(robCurrent, skipCurrent);
-
-    return memo[index];
-}
-
-int rob(vector<int> &nums)
-{
-    if (nums.size() == 1)
-        return nums[0];
-    if (nums.size() == 2)
-        return max(nums[0], nums[1]);
-
-    int ans = 0;
-    unordered_map<int, int> memo;
-    ans = robbingMemo(0, nums, memo);
-
-    return ans;
-}
+};
